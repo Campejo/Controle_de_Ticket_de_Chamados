dc30e2226bbb7e68f726901334f48d9ffe92d8e6
Thiago Campello <campellothiago@hotmail.com>
2025-02-05 20:49:23 -0300

    fix: fixing the security

--- project/Main.py
+++ project/Main.py
@@ -1,11 +1,7 @@
 import tkinter as tk
 from datetime import datetime
-import psycopg2
-# from project.DB.AppDB import AppDB
+from model.AppDB import AppDB
 from tkinter import ttk, messagebox
-
-from psycopg2 import sql
-
 
 class MainApp:
     def __init__(self, root):
@@ -26,51 +22,58 @@ class MainApp:
                         font=("Arial", 10))
         style.configure("Treeview.Heading", font=("Arial", 12, "bold"))
 
+        # Frame para as labels e entradas de texto
+        frame_labels = tk.Frame(root, bg="#F7F7F7")
+        frame_labels.pack(pady=10, anchor="center", padx=20)
+
         # Labels e Entrys
-        self.lblCodigo = tk.Label(root, text="ID do Chamado:", bg="#F7F7F7", font=("Arial", 12))
-        self.lblCodigo.grid(row=0, column=0, padx=20, pady=10, sticky="w")
-        self.txtCodigo = tk.Entry(root, font=("Arial", 12), bd=2)
-        self.txtCodigo.grid(row=0, column=1, padx=10, pady=10, sticky="we")
+        self.lblCodigo = tk.Label(frame_labels, text="ID do Chamado:", bg="#F7F7F7", font=("Arial", 12))
+        self.lblCodigo.pack(anchor="w")
+        self.txtCodigo = tk.Entry(frame_labels, font=("Arial", 12), bd=2)
+        self.txtCodigo.pack(fill="x", pady=5)
 
-        self.lblLocal = tk.Label(root, text="Local:", bg="#F7F7F7", font=("Arial", 12))
-        self.lblLocal.grid(row=1, column=0, padx=20, pady=10, sticky="w")
-        self.txtLocal = tk.Entry(root, font=("Arial", 12), bd=2)
-        self.txtLocal.grid(row=1, column=1, padx=10, pady=10, sticky="we")
+        self.lblLocal = tk.Label(frame_labels, text="Local:", bg="#F7F7F7", font=("Arial", 12))
+        self.lblLocal.pack(anchor="w")
+        self.txtLocal = tk.Entry(frame_labels, font=("Arial", 12), bd=2)
+        self.txtLocal.pack(fill="x", pady=5)
 
-        self.lblDescricao = tk.Label(root, text="Descrição:", bg="#F7F7F7", font=("Arial", 12))
-        self.lblDescricao.grid(row=2, column=0, padx=20, pady=10, sticky="w")
-        self.txtDescricao = tk.Entry(root, font=("Arial", 12), bd=2)
-        self.txtDescricao.grid(row=2, column=1, padx=10, pady=10, sticky="we")
+        self.lblDescricao = tk.Label(frame_labels, text="Descrição:", bg="#F7F7F7", font=("Arial", 12))
+        self.lblDescricao.pack(anchor="w")
+        self.txtDescricao = tk.Entry(frame_labels, font=("Arial", 12), bd=2)
+        self.txtDescricao.pack(fill="x", pady=5)
 
-        # Botões
-        self.btnCadastrar = tk.Button(root, text="Cadastrar", bg="#4CAF50", fg="white", font=("Arial", 12),
+        # Frame para os botões
+        frame_botoes = tk.Frame(root, bg="#F7F7F7")
+        frame_botoes.pack(pady=10)
+
+        # Botões dispostos horizontalmente
+        self.btnCadastrar = tk.Button(frame_botoes, text="Cadastrar", bg="#28a745", fg="white", font=("Arial", 12),
                                       command=self.fCadastrarChamado, cursor="hand2")
-        self.btnCadastrar.grid(row=0, column=2, padx=5, pady=5)
+        self.btnCadastrar.pack(side=tk.LEFT, padx=10)
 
-
-        self.btnExcluir = tk.Button(root, text="Excluir", bg="#F44336", fg="white", font=("Arial", 12),
-                                    command=self.fDeletarChamado, cursor="hand2")
-        self.btnExcluir.grid(row=4, column=2, padx=5, pady=5)
-
-
-        self.btnLimpar = tk.Button(root, text="Limpar", bg="#FF9800", fg="white", font=("Arial", 12),
-                                   command=self.fLimparTela, cursor="hand2")
-        self.btnLimpar.grid(row=2, column=2, padx=5, pady=5)
-
-
-        self.btnAtualizar = tk.Button(root, text="Atualizar", bg="#2196F3", fg="white", font=("Arial", 12),
+        self.btnAtualizar = tk.Button(frame_botoes, text="Atualizar", bg="#007bff", fg="white", font=("Arial", 12),
                                       command=self.fAtualizarChamado, cursor="hand2")
-        self.btnAtualizar.grid(row=1, column=2, padx=5, pady=5)
+        self.btnAtualizar.pack(side=tk.LEFT, padx=10)
 
+        self.btnLimpar = tk.Button(frame_botoes, text="Limpar Campos", bg="#fd7e14", fg="white", font=("Arial", 12),
+                                   command=self.fLimparTela, cursor="hand2")
+        self.btnLimpar.pack(side=tk.LEFT, padx=10)
 
-        self.btnFinalizar = tk.Button(root, text="Finalizar", bg="#9C27B0", fg="white", font=("Arial", 12),
+        self.btnFinalizar = tk.Button(frame_botoes, text="Finalizar", bg="#6f42c1", fg="white", font=("Arial", 12),
                                       command=self.fFinalizarChamado, cursor="hand2")
-        self.btnFinalizar.grid(row=3, column=2, padx=5, pady=5)
+        self.btnFinalizar.pack(side=tk.LEFT, padx=10)
 
+        self.btnExcluir = tk.Button(frame_botoes, text="Excluir", bg="#dc3545", fg="white", font=("Arial", 12),
+                                    command=self.fDeletarChamado, cursor="hand2")
+        self.btnExcluir.pack(side=tk.LEFT, padx=10)
+
+        # Frame para a Treeview e Scrollbar
+        frame_tree = tk.Frame(root)
+        frame_tree.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
         # Treeview
         self.dadosColunas = ("CODIGO", "LOCAL", "DESCRICAO", "ABERTURA", "FECHAMENTO")
-        self.treeChamados = ttk.Treeview(root, columns=self.dadosColunas, show="headings", height=10)
+        self.treeChamados = ttk.Treeview(frame_tree, columns=self.dadosColunas, show="headings", height=10)
 
         # Definindo as colunas
         self.treeChamados.heading("CODIGO", text="Código")
@@ -85,19 +88,15 @@ class MainApp:
         self.treeChamados.column("ABERTURA", width=100, anchor="center")
         self.treeChamados.column("FECHAMENTO", width=100, anchor="center")
 
-        self.treeChamados.grid(row=3, column=0, columnspan=2, padx=20, pady=20, sticky="nsew")
+        self.treeChamados.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Barra de rolagem para a Treeview
+        self.scrollbar = ttk.Scrollbar(frame_tree, orient="vertical", command=self.treeChamados.yview)
+        self.treeChamados.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         # Bind para apresentar dados selecionados
         self.treeChamados.bind("<<TreeviewSelect>>", self.fApresentarChamadosSelecionados)
-
-        # Barra de rolagem para a Treeview
-        self.scrollbar = ttk.Scrollbar(root, orient="vertical", command=self.treeChamados.yview)
-        self.treeChamados.configure(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.grid(row=3, column=1, sticky="nse")
-
-        # Configurando grid para expandir
-        root.grid_columnconfigure(1, weight=1)
-        root.grid_rowconfigure(3, weight=1)
 
         # Carregar dados iniciais
         self.fCarregarDadosIniciais()
@@ -210,126 +209,10 @@ class MainApp:
             print(f"Erro: {e}")
 
 
-
-class AppDB:
-
-    def __init__(self):
-        self.url = "dbname='postgres' user='postgres' host='localhost' password='131627'"
-        self.connection = None
-
-    # ABRIR CONEXÃO COM O BANCO
-    def abrirConexao(self):
-        try:
-            self.connection = psycopg2.connect(self.url)
-            #print("Conexão aberta!\n")
-        except Exception as e:
-            print(e)
-
-    # FECHAR CONEXÃO COM O BANCO
-    def fecharConexao(self):
-        try:
-            if self.connection:
-                self.connection.close()
-                #print("\nConexão fechada!")
-        except Exception as e:
-            print(e)
-
-    # SELECIONA TODOS OS DADOS DA TABELA CHAMADO
-    def selecionarDados(self):
-        query = sql.SQL("SELECT * FROM \"CHAMADO\"")
-
-        try:
-            self.abrirConexao()
-            with self.connection.cursor() as cursor:
-                cursor.execute(query)
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(f"ID: {row[0]}\nLocal: {row[1]}\nDescrição: {row[2]}\nAbertura: {row[3]}\nFechamento: {row[4]}\n")
-                return rows
-        except Exception as e:
-            print(e)
-            return None
-        finally:
-            self.fecharConexao()
-
-    # INSERIR DADOS NA TABELA CHAMADO
-    def inserirChamado(self, id, local, descricao, abertura):
-        insert_query = sql.SQL("INSERT INTO \"CHAMADO\" (\"CODIGO\", \"LOCAL\", \"DESCRICAO\", \"ABERTURA\") VALUES (%s, %s, %s, %s)")
-
-        try:
-            self.abrirConexao()
-            with self.connection.cursor() as cursor:
-                cursor.execute(insert_query, (id, local, descricao, abertura))
-                self.connection.commit()
-                print("Registro inserido com sucesso!")
-        except Exception as e:
-            print("Falha ao inserir os dados na tabela")
-            print(e)
-        finally:
-            self.fecharConexao()
-
-    # ATUALIZAR DADOS NA TABELA CHAMADO
-    def atualizarChamado(self, id, local_novo, descricao_nova):
-        select_query = sql.SQL("SELECT * FROM \"CHAMADO\" WHERE \"CODIGO\" = %s")
-        update_query = sql.SQL("UPDATE \"CHAMADO\" SET \"LOCAL\" = %s, \"DESCRICAO\" = %s WHERE \"CODIGO\" = %s")
-
-        try:
-            self.abrirConexao()
-            with self.connection.cursor() as cursor:
-                cursor.execute(select_query, (id,))
-                print("Seleção antes da atualização.")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(f"Código: {row[0]}\nLocal: {row[1]}\nDescrição: {row[2]}")
-
-                cursor.execute(update_query, (local_novo, descricao_nova, id))
-                self.connection.commit()
-                print("Atualização realizada com sucesso!")
-
-                cursor.execute(select_query, (id,))
-                print("Seleção depois da atualização.")
-                rows = cursor.fetchall()
-                for row in rows:
-                    print(f"Código: {row[0]}\nLocal: {row[1]}\nDescrição: {row[2]}")
-        except Exception as e:
-            print(e)
-        finally:
-            self.fecharConexao()
-
-    # FINALIZAR O TICKET DA TABELA DE CHAMADOS
-    def finalizarChamado(self, id):
-        query = sql.SQL("UPDATE \"CHAMADO\" SET \"FECHAMENTO\" = %s WHERE \"CODIGO\" = %s")
-
-        try:
-            self.abrirConexao()
-            with self.connection.cursor() as cursor:
-                now = datetime.now().strftime("%Y-%m-%d")
-                cursor.execute(query, (now, id))
-                self.connection.commit()
-                print("Chamado finalizado.")
-        except Exception as e:
-            print(e)
-        finally:
-            self.fecharConexao()
-
-    # DELETAR CHAMADO DO BANCO DE DADOS
-    def deletarChamado(self, id):
-        delete_query = sql.SQL("DELETE FROM \"CHAMADO\" WHERE \"CODIGO\" = %s")
-
-        try:
-            self.abrirConexao()
-            with self.connection.cursor() as cursor:
-                cursor.execute(delete_query, (id,))
-                self.connection.commit()
-                print("Chamado deletado!")
-        except Exception as e:
-            print(e)
-        finally:
-            self.fecharConexao()
-
 # Inicialização da interface
 if __name__ == "__main__":
     root = tk.Tk()
     app = MainApp(root)
     root.mainloop()
+
 
